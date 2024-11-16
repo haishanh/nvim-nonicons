b338d6165f587d50854e736a79f8e66b5edfd177
haishan <haishan.han@nike.com>
2024-11-16 15:54:55 +0800

    nvim-nonicons

--- lua/nvim-nonicons.lua
+++ lua/nvim-nonicons.lua
@@ -1,8 +1,8 @@
 local icons = require("nvim-nonicons.mapping")
 
-if (vim.g.override_nvim_web_devicons ~= false) then
-  require("nvim-web-devicons.override")
-end
+-- if (vim.g.override_nvim_web_devicons ~= false) then
+--   require("nvim-web-devicons.override")
+-- end
 
 local function get(name)
   return vim.fn.nr2char(icons[name])
